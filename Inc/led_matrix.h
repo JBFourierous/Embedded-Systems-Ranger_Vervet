bfa2de770970011b0c113fbed1d2ba555317865b
JBFourierous <44525521+JBFourierous@users.noreply.github.com>
2019-07-28 18:10:47 +0200

    Led matrix interface

---
+++ Inc/led_matrix.h
@@ -0,0 +1,24 @@
+/*
+ * led_matrix.h
+ *
+ *  Created on: 11 lug 2019
+ *      Author: JBFourierous
+ */
+
+#ifndef LED_MATRIX_H_
+#define LED_MATRIX_H_
+
+#include "common.h"
+#include "stm32f4xx_hal_tim.h"
+
+#define N_LEDS 32
+#define INTENSITY 50
+
+static const uint8_t WHITE[] = {50, 50, 50};
+static const uint8_t BLACK[] = {0, 0, 0};
+
+void turn_on_led_matrix();
+void turn_off_led_matrix();
+void init_led_matrix();
+
+#endif /* LED_MATRIX_H_ */
