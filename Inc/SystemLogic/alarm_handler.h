889041cc0ebcea15a0b0dcb942e6c262c938ca86
JBFourierous <44525521+JBFourierous@users.noreply.github.com>
2019-08-19 11:01:43 +0200

    Alarm handler interface

--- Inc/SystemLogic/alarm_handler.h
+++ Inc/SystemLogic/alarm_handler.h
@@ -1,1 +1,14 @@
+#ifndef ALARM_HANDLER_H_
+#define ALARM_HANDLER_H_
 
+#include "common.h"
+#include "motor_driver.h"
+#include "speaker_alarm.h"
+#include "led_matrix.h"
+#include "microphone.h"
+#include "GSM_board.h"
+#include "camera.h"
+
+void handle_alarm(char alarm_type);
+
+#endif /* ALARM_HANDLER_H_ */
