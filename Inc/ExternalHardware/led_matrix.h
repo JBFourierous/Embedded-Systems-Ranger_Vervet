3db0948b2d728cf67507ce5cc107c114bdfe2afb
JBFourierous <44525521+JBFourierous@users.noreply.github.com>
2019-08-19 10:49:48 +0200

    Led matrix driver interface

--- Inc/ExternalHardware/led_matrix.h
+++ Inc/ExternalHardware/led_matrix.h
@@ -1,1 +1,29 @@
+/*
+ * led_matrix.h
+ *
+ *  Created on: 11 lug 2019
+ *      Author: JBFourierous
+ */
+
+#ifndef LED_MATRIX_H_
+#define LED_MATRIX_H_
+
+#include "common.h"
+#include "stm32f4xx_hal_tim.h"
+#include "math.h"
+
+#define N_LEDS 32
+#define INTENSITY 50
+#define LED_BYTE 24
+#define H_VAL 75
+#define L_VAL 30
+
+static const uint8_t WHITE[] = {50, 50, 50};
+static const uint8_t BLACK[] = {0, 0, 0};
+
+void turn_on_led_matrix();
+void turn_off_led_matrix();
+void init_led_matrix();
+
+#endif /* LED_MATRIX_H_ */
 
